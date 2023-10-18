ccaa6a618ed8c22869eca2cb833faeb06849f185
Safal Neupane <safal.npane@gmail.com>
2023-10-18 18:04:05 +1100

    chore: add screenshots and README

---
+++ README.md
@@ -0,0 +1,17 @@
+# NeoVim config
+Simple and yet powerful Neovim config.
+
+## Editing
+![](screenshots/editing.png)
+
+## File Browse
+I like the `:Explore` than the `Telescope`. Just a personal preference.
+![](screenshots/browse.png)
+
+## Fuzzy-find
+Nothing beats `Telescope` on this one.
+![](screenshots/file_find.png)
+
+## Git
+The combination of `vim-fugitive` and `git-blame` is *just* perfect for my `git` needs.
+![](screenshots/git.png)
--- init.lua
+++ init.lua
@@ -4,32 +4,3 @@ require('filetype')
 require('base')
 require('plugins')
 require('keymaps')
-
-local status, ts = pcall(require, "nvim-treesitter.configs")
-if (not status) then return end
-
-ts.setup {
-  highlight = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-  },
-  ensure_installed = {
-    "markdown",
-    "markdown_inline",
-    "lua",
-    "python",
-    "go",
-    "gitcommit",
-    "typescript",
-    "json",
-    "css",
-    "html",
-    "javascript",
-    "yaml",
-    "tsx",
-    "rust",
-    "c"
-  },
-}
